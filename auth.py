f055e98ec1f002a35a09347a0dab732b4c1eeeb8
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-02-05 14:35:53 +0100

    Store API_KEY in secrets file

--- auth.py
+++ auth.py
@@ -1,18 +1,25 @@
 import json
 import uuid
 from flask import current_app, request
-from pathlib import Path
 from functools import wraps
 
 
+SECRETS_PATH = ".secrets.json"
+
+
 def init_auth():
-    """ Get the API_KEY from file or generate one """
+    """
+        Makes sure the app has a API_KEY in config.
+        API_KEY is stored in the secrets file.
+        A new one is generated if there is no key.
+    """
 
     try:
         key = load_api_key()
     except FileNotFoundError or ValueError:
         current_app.logger.info("Could not load API_KEY from .secrets")
         key = create_api_key()
+        store_api_key(key)
 
     current_app.config["API_KEY"] = key
     current_app.logger.info(f"Key set to {key}")
@@ -20,10 +27,15 @@ def init_auth():
 
 def load_api_key():
     """ load api_key from secrets file """
-    fp = Path(".secrets")
-    with open(fp) as file:
+    with open(SECRETS_PATH, "r") as file:
         data = json.load(file)
         return data["api_key"]
+
+
+def store_api_key(key):
+    """ store api_key in secrets file """
+    with open(SECRETS_PATH, "w") as file:
+        json.dump({"api_key": key}, file)
 
 
 def create_api_key():
