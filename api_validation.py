724266627c76d350ea05940c3326303b8c100ece
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-02-05 15:49:57 +0100

    validation for device api

---
+++ api_validation.py
@@ -0,0 +1,27 @@
+from functools import wraps
+from flask import request
+from model import get_device_names
+
+
+def validated(function):
+    """ wrapper for API requests """
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+
+        data = request.get_json()
+
+        # check that json is present
+        if not data:
+            return {"message": "missing json data"}, 400
+
+        # check that device key is present
+        if "device" not in data:
+            return {"message": "missing 'device' key from json data"}, 400
+
+        # check that device is valid name
+        if data["device"] not in get_device_names():
+            return {"message": "invalid device"}, 400
+
+        return function(*args, **kwargs)
+
+    return decorated_function
