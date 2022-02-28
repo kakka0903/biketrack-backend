05dd8a90ffc80d47df63173b513b533fc84fcb63
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-02-28 20:39:06 +0100

    bugfix. thows error if UNIVERSAL_API_KEY is not set

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 app.register_blueprint(api)
 CORS(app)
 
-
-if os.environ["UNIVERSAL_API_KEY"]:
-    app.logger.warning("Don't use UNIVERSAL_API_KET in prod")
-    app.logger.warning("Anyone with access to the key can post fake data")
+try:
+    if os.environ["UNIVERSAL_API_KEY"]:
+        app.logger.warning("Don't use UNIVERSAL_API_KET in prod")
+        app.logger.warning("Anyone with access to the key can post fake data")
+except KeyError:
+    pass
 
 
 @app.get("/")
