c6c4922ceb80d75d8e11de9fd51a7ad799d8f2c1
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-03-05 22:06:42 +0100

    add: config files
    config.cfg is included in version control
    config.local.cfg is not included and is used to set environment specific config values

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def create_app():
     from model import db
     db.init_app(app)
 
+    app.config.from_pyfile('config.cfg')
+    app.config.from_pyfile('config.local.cfg', silent=True)
+
     try:
         if os.environ["UNIVERSAL_API_KEY"]:
             app.logger.warning("Don't use UNIVERSAL_API_KET in prod")
