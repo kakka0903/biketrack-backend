b1cd441d68166dd7e36236f54226138f941e8b98
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-03-05 22:20:56 +0100

    fix: load config before db

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_pyfile('config.cfg')
+    app.config.from_pyfile('config.local.cfg', silent=True)
+
     from api import api
     app.register_blueprint(api)
 
     from model import db
     db.init_app(app)
-
-    app.config.from_pyfile('config.cfg')
-    app.config.from_pyfile('config.local.cfg', silent=True)
 
     return app
