7d2c4c9c9cb4843d25460b0fcadd2211718e9808
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-03-06 23:53:12 +0100

    refactor: api uses db models

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from decorators import device_authorized, valid_device
-from model import devices, get_device, Device
+from model import DeviceData, get_device, Device, db
 from flask_cors import CORS
 
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -12,8 +12,8 @@ CORS(api)
 @valid_device  # make sure device is valid (aka registered)
 def update():
     """ Device updates are posted here """
-    required_keys = ["device_name", "location_fix", "lat", "lon",
-                     "location_type", "time", "date", "battery_voltage"]
+    required_keys = ["device_name", "lat", "lon",
+                     "battery_voltage", "battery_percentage"]
 
     # check that all required keys are present
     data = request.get_json()
@@ -21,11 +21,15 @@ def update():
         if key not in data:
             return {"message": f"missing '{key}' key"}, 400
 
-    # update device data
+    # get device and create record
     device = get_device(data["device_name"])
+    update_record = DeviceData(device=device)
+
     for key in required_keys:
         if key != "device_name":
-            setattr(device.data, key, data[key])
+            setattr(update_record, key, data[key])
+
+    db.session.commit()
 
     # return settings to the device
     return {'message': 'update successful!'}, 200
@@ -38,7 +42,7 @@ def latest():
     data = request.get_json()
     device = get_device(data["device_name"])
 
-    return {device.name: device.serialize()}
+    return {device.name: device}
 
 
 @api.post("/change-settings")
@@ -60,8 +64,9 @@ def create_device():
     """ create a new device """
     try:
         data = request.get_json()
-        device = Device(data["device_name"])
-        devices.append(device)
-        return {"message": "device created successfully!", "api_key": device.auth.api_key}, 200
+        device = Device(name=data["device_name"])
+        db.session.add(device)
+        db.session.commit()
+        return {"message": "device created successfully!", "api_key": device.api_key}, 200
     except KeyError as error:
         return {"message": f"missing key {error}"}, 400
