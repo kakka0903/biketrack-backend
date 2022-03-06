438136b241f9d4bc065c62bc5bdc9f6b02e2b445
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-03-06 12:04:46 +0100

    replace migrate models to database models

--- model.py
+++ model.py
@@ -4,30 +4,28 @@ from sqlalchemy_utils import UUIDType
 from flask.cli import with_appcontext
 import uuid
 
-devices = []
 db = SQLAlchemy()
 
 
 def get_device_names():
+    devices = Device.query.all()
     return [device.name for device in devices]
 
 
 def get_device(name):
-    for device in devices:
-        if device.name == name:
-            return device
+    return Device.query.filter_by(name='koga-miyata').first()
 
 
-class DeviceModel(db.Model):
+class Device(db.Model):
     """ Stores information about devices """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     api_key = db.Column(UUIDType, nullable=False, default=uuid.uuid4)
-    data = db.relationship('DeviceDataModel', lazy=True)
-    settings = db.relationship('DeviceSettingsModel', lazy=True)
+    data = db.relationship('DeviceData', lazy=True)
+    settings = db.relationship('DeviceSettings', lazy=True)
 
 
-class DeviceDataModel(db.Model):
+class DeviceData(db.Model):
     """ Stores data provided by device """
     id = db.Column(db.Integer, primary_key=True)
     lon = db.Column(db.Float)
@@ -35,51 +33,17 @@ class DeviceDataModel(db.Model):
     battery_voltage = db.Column(db.Float)
     battery_percentage = db.Column(db.Float)
 
-    device_id = db.Column(db.Integer, db.ForeignKey('device_model.id'),
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'),
                           nullable=False)
 
 
-class DeviceSettingsModel(db.Model):
+class DeviceSettings(db.Model):
     """ Stores device settings """
     id = db.Column(db.Integer, primary_key=True)
     update_interval = db.Column(db.Integer, nullable=False, default=3600)
 
-    device_id = db.Column(db.Integer, db.ForeignKey('device_model.id'),
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'),
                           nullable=False)
-
-
-class Device():
-    def __init__(self, name):
-        self.name = name
-        self.data = DeviceData()
-        self.settings = DeviceSettings()
-        self.auth = DeviceAuth()
-
-    def serialize(self):
-        return {
-            "name": self.name,
-            "data": self.data.__dict__,
-            "settings": self.settings.__dict__,
-            "auth": self.auth.__dict__,
-        }
-
-
-class DeviceData():
-    def __init__(self):
-        self.lat = None
-        self.lon = None
-        self.battery_voltage = None
-        self.battery_percentage = None
-
-
-class DeviceSettings():
-    def __init__(self):
-        self.update_interval = 0
-
-
-class DeviceAuth():
-    def __init__(self):
-        self.api_key = str(uuid.uuid4())
 
 
 @click.command("init-db")
