07431b759698867dfa1340a53188e0f5f52a9d5a
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-03-07 10:53:39 +0100

    refactor: remove unnecessary code form auth_device

--- decorators.py
+++ decorators.py
@@ -3,8 +3,11 @@ from functools import wraps
 from model import get_device
 
 
-    """ wrapper to reject requests that are not authenticated """
 def auth_device(function):
+    """
+        makes sure the device is authorized before allowing update
+        assumes "device" kwarg is the device row object
+    """
     @wraps(function)
     def decorated_function(*args, **kwargs):
         data = request.get_json()
@@ -12,21 +15,12 @@ def auth_device(function):
         if not data:
             return {"message": "no json data present"}, 400
 
-        # make sure device name is present
-        if "device_name" not in data:
-            return {"message": "device_name not in json"}, 400
-
         # make auth data is present
         if not data or "api_key" not in data:
             return {"message": "api_key not in json"}, 400
 
-        # make sure device exists
-        device = get_device(data["device_name"])
-        if not device:
-            return {"message": "invalid device_name or api_key"}, 400
-
         # check if api_key matches device registered api_key
-        device = get_device(data["device_name"])
+        device = kwargs["device"]
         validated = data["api_key"] == device.api_key
 
         # check if api_key matches universal apikey
